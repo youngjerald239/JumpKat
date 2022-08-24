d53d502c9f6b679f74a46c424aac0c403292a37e
youngjerald239 <youngjerald239@gmail.com>
2022-08-23 22:29:18 -0500

    added csv files and created game_data.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame, sys
 from settings import *
+from level import Level
+from game_data import level_0
 
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
+level = Level(level_0,screen)
 
 while True:
     for event in pygame.event.get():
@@ -13,6 +16,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
