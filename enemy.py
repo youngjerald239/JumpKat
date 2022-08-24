56c21b534715e9a17cd8932080551c4864999db5
youngjerald239 <youngjerald239@gmail.com>
2022-08-24 14:36:54 -0500

    placed enemies on ground

--- enemy.py
+++ enemy.py
@@ -4,3 +4,4 @@ from tiles import AnimatedTile
 class Enemy(AnimatedTile):
     def __init__(self,size,x,y):
         super().__init__(size,x,y,'./graphics/enemy/run')
+        self.rect.y += size - self.image.get_size()[1]
