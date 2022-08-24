85323373704730d1fbfc73efe6763e99abfaa1f2
youngjerald239 <youngjerald239@gmail.com>
2022-08-23 23:06:12 -0500

    added row and tiles

--- level.py
+++ level.py
@@ -1,13 +1,30 @@
 import pygame
 from support import import_csv_layout
+from settings import tile_size
+from tiles import Tile
 
 class Level:
     def __init__(self,level_data,surface):
         self.display_surface = surface
 
         terrain_layout = import_csv_layout(level_data['terrain'])
-        print(terrain_layout)
+        self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
+
+    def create_tile_group(self,layout,type):
+        sprite_group = pygame.sprite.Group()
+
+        for row_index, row in enumerate(layout):
+            for col_index,val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        sprite = Tile(tile_size,x,y)
+                        sprite_group.add(sprite)
+
+        return sprite_group
 
     def run(self):
         # run the full game
-        pass
+        self.terrain_sprites.draw()
