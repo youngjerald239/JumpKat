bbeb619cec25f0c8aa3b6759ae5a8c89cbc2f877
youngjerald239 <youngjerald239@gmail.com>
2022-08-24 12:49:16 -0500

    added import folder path

--- support.py
+++ support.py
@@ -3,10 +3,16 @@ from settings import tile_size
 from os import walk
 import pygame
 
-
 def import_folder(path):
-    for information in walk(path):
-        print(information)
+    surface_list = []
+
+    for _,__,image_files in walk(path):
+        for image in image_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+
+    return surface_list
 
 def import_csv_layout(path):
     terrain_map = []
