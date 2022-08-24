18cf4ffb1f38061254202827b458f91de37fe854
youngjerald239 <youngjerald239@gmail.com>
2022-08-24 12:23:06 -0500

    offset_y to place crates on ground

--- tiles.py
+++ tiles.py
@@ -17,3 +17,5 @@ class StaticTile(Tile):
 class Crate(StaticTile):
     def __init__(self,size,x,y):
         super().__init__(size,x,y,pygame.image.load('./graphics/terrain/crate.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x,offset_y))
