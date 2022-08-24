3b44d8bed08dc66c6c6cfa685c618c5a8f67ea16
youngjerald239 <youngjerald239@gmail.com>
2022-08-24 12:08:09 -0500

    added grass tile and imported image from csv

--- level.py
+++ level.py
@@ -7,11 +7,15 @@ class Level:
     def __init__(self,level_data,surface):
         # general setup
         self.display_surface = surface
-        self.world_shift = 0
+        self.world_shift = -5
 
         #terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
+
+        # grass setup
+        grass_layout = import_csv_layout(level_data['grass'])
+        self.grass_sprites = self.create_tile_group(grass_layout,'grass')
 
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -26,7 +30,13 @@ class Level:
                         terrain_tile_list = import_cut_graphics('./graphics/terrain/terrain_tiles.png')
                         tile_surface = terrain_tile_list[int(val)]
                         sprite = StaticTile(tile_size,x,y,tile_surface)
-                        sprite_group.add(sprite)
+                        
+                    if type == 'grass':
+                        grass_tile_list = import_cut_graphics('./graphics/decoration/grass/grass.png')
+                        tile_surface = grass_tile_list[int(val)]
+                        sprite = StaticTile(tile_size,x,y,tile_surface)
+
+                    sprite_group.add(sprite)
 
         return sprite_group
 
@@ -34,3 +44,7 @@ class Level:
         # run the full game
         self.terrain_sprites.draw(self.display_surface)
         self.terrain_sprites.update(self.world_shift)
+
+        # grass
+        self.grass_sprites.draw(self.display_surface)
+        self.grass_sprites.update(self.world_shift)
