41abc7586e5ba1550dd0df9dc4700eb70965b573
youngjerald239 <youngjerald239@gmail.com>
2022-08-24 15:42:37 -0500

    added middle sky

--- decoration.py
+++ decoration.py
@@ -17,4 +17,7 @@ class Sky:
     def draw(self,surface):
         for row in range(vertical_tile_number):
             y = row * tile_size
-            surface.blit(self.top,(0,y))
+            if row < self.horizon:
+                surface.blit(self.top,(0,y))
+            elif row == self.horizon:
+                surface.blit(self.middle,(0,y))
