e612bf4642036bba8ecf1ef521dea6767923f526
youngjerald239 <youngjerald239@gmail.com>
2022-08-26 20:56:33 -0500

    added level 3

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ class Game:
         self.status = 'level'
 
     def create_overworld(self,current_level,new_max_level):
+        if new_max_level > self.max_level:
+            self.max_level = new_max_level
         self.overworld = Overworld(current_level,self.max_level,screen,self.create_level)
         self.status = 'overworld'
     def run(self):
