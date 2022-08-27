bcf00c5c441798905abb8ee9fa794e8dc0adc711
youngjerald239 <youngjerald239@gmail.com>
2022-08-26 22:53:59 -0500

    added hat png

--- overworld.py
+++ overworld.py
@@ -20,8 +20,7 @@ class Icon(pygame.sprite.Sprite):
     def __init__(self,pos):
         super().__init__()
         self.pos = pos
-        self.image = pygame.Surface((20,20))
-        self.image.fill('blue')
+        self.image = pygame.image.load('./graphics/overworld/hat.png')
         self.rect = self.image.get_rect(center = pos)
 
     def update(self):
